ed8a395071790a3630cab50b59a9b1ff2bce5750
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2024-06-26 17:30:46 +0200

    docs: add comments to HelloWorldCommand

--- src/UserInterface/Cli/Command/HelloWorldCommand.php
+++ src/UserInterface/Cli/Command/HelloWorldCommand.php
@@ -7,6 +7,18 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+
+/*
+   Il componente Console di Symfony facilita la creazione di interfacce a riga di comando.
+   Per creare un comando, si deve definire una classe che estende `Symfony\Component\Console\Command\Command`
+   Questa classe deve implementare il metodo `configure()` per definire i parametri e le opzioni del comando,
+   e il metodo `execute()` per la logica applicativa.
+   Il comando è registrato come servizio con il tag `console.command`.
+   Una volta registrato, il comando può essere eseguito dalla riga di comando
+   utilizzando il comando `bin/console`.
+ * */
+
+//Questo è un comando nel contesto delle applicazioni a riga di comando
 final class HelloWorldCommand extends Command
 {
     public function __construct()
@@ -14,11 +26,13 @@ final class HelloWorldCommand extends Command
         parent::__construct('app:helloworld');
     }
 
+    //Qui è dove si definiscono proprietà del comando (argomenti, etc.)
     protected function configure(): void
     {
         $this->addArgument('name', InputArgument::REQUIRED, 'Name of the one running the example command.');
     }
 
+    //Qui è dove il comando viene eseguito
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $name = $input->getArgument('name');
@@ -26,5 +40,10 @@ final class HelloWorldCommand extends Command
         $output->writeln("<info>Hello world, my name is $name.</info>");
 
         return self::SUCCESS;
+
+        /*
+        Approfondimento:
+        - execute() e configure() sono metodi di cui viene fatto overriding: vediamo che significa
+         * */
     }
 }
